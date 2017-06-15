c78be4f503e96d45b957882bfc26af3f8c20f9de
rfornes <ricardo@tvpage.com>
2017-06-15 11:46:28 -0700

    Check first if there's files before renaming

--- rename.js
+++ rename.js
@@ -3,17 +3,19 @@ var replaceExt = require('replace-ext');
 
 var filesPath = "public/tvpwidget";
 fs.readdir(filesPath, function(err, items) {
-    for (var i=0; i<items.length; i++) {
-        var filePath = filesPath + "/" + items[i] + "/index.html";
-        
-        if (fs.existsSync(filePath)) {
-            var newFilePath = replaceExt(filePath, '.js');
-            console.log("replacing ", filePath);
-            fs.rename(filePath, newFilePath, function(err) {
-                if (err) console.log('Something went bad ' + err);
-            });
-        } else {
-        console.warn("no html file found ", filePath);
+    if (items && items.length) {
+        for (var i=0; i<items.length; i++) {
+            var filePath = filesPath + "/" + items[i] + "/index.html";
+            
+            if (fs.existsSync(filePath)) {
+                var newFilePath = replaceExt(filePath, '.js');
+                console.log("replacing ", filePath);
+                fs.rename(filePath, newFilePath, function(err) {
+                    if (err) console.log('Something went bad ' + err);
+                });
+            } else {
+                console.warn("no html file found ", filePath);
+            }
         }
-    };
+    }
 });
